985c3f22c277d058e5a5d6c49558fbf6feb8dcb7
User <user@ds-MacBook-Pro.local>
2020-04-14 20:28:05 +0300

    clock

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
-import Navigation from './src/Navigation/index'
-import {AppLoading } from "expo";
-import * as Font from 'expo-font';
+import React, { Component } from "react";
+import Navigation from "./src/Navigation/index";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      fontLoaded: false
+      fontLoaded: false,
     };
   }
   async componentDidMount() {
     await Font.loadAsync({
-      "OS_Regular" : require("./assets/fonts/OpenSans-Regular.ttf"),
-      "OS_Bold" : require("./assets/fonts/OpenSans-Bold.ttf"),
-      "OS_Light" : require("./assets/fonts/OpenSans-Light.ttf"),
-      "OS_SB" : require("./assets/fonts/OpenSans-SemiBold.ttf"),
+      OS_Regular: require("./assets/fonts/OpenSans-Regular.ttf"),
+      OS_Bold: require("./assets/fonts/OpenSans-Bold.ttf"),
+      OS_Light: require("./assets/fonts/OpenSans-Light.ttf"),
+      OS_SB: require("./assets/fonts/OpenSans-SemiBold.ttf"),
     });
     this.setState({ fontLoaded: true });
   }
--- src/Screen/tovo/index.js
+++ src/Screen/tovo/index.js
@@ -8,6 +8,7 @@ import {
   Button,
   Linking,
   Navigator,
+  Vibration,
 } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
 import Header from "../../Component/Header/HeaderClose";
@@ -16,9 +17,10 @@ import Color from "../../Config/Color";
 import Style from "../Details/style";
 import TextInputView from "../../Component/TextInput/index";
 import ButtonCom from "../../Component/Button/index";
-import { Updates } from "expo";
-
+import Expo, { Notifications, Updates } from "expo";
+import Constants from "expo-constants";
 import styleText from "../../Component/TextInput/style";
+import * as Permissions from "expo-permissions";
 
 export default class index extends Component {
   state = {
@@ -29,6 +31,81 @@ export default class index extends Component {
     phone: "",
     email: "",
     ID: "",
+    expoPushToken: "",
+    notification: {},
+  };
+
+  registerForPushNotificationsAsync = async () => {
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+      );
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Permissions.askAsync(
+          Permissions.NOTIFICATIONS
+        );
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = await Notifications.getExpoPushTokenAsync();
+      console.log(token);
+      this.setState({ expoPushToken: token });
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    if (Platform.OS === "android") {
+      Notifications.createChannelAndroidAsync("default", {
+        name: "default",
+        sound: true,
+        priority: "max",
+        vibrate: [0, 250, 250, 250],
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.registerForPushNotificationsAsync();
+
+    // Handle notifications that are received or selected while the app
+    // is open. If the app was closed and then opened by tapping the
+    // notification (rather than just tapping the app icon to open it),
+    // this function will fire on the next tick after the app starts
+    // with the notification data.
+    this._notificationSubscription = Notifications.addListener(
+      this._handleNotification
+    );
+  }
+
+  _handleNotification = (notification) => {
+    Vibration.vibrate();
+    console.log(notification);
+    this.setState({ notification: notification });
+  };
+
+  // Can use this function below, OR use Expo's Push Notification Tool-> https://expo.io/dashboard/notifications
+  sendPushNotification = async (title, body) => {
+    const message = {
+      to: this.state.expoPushToken,
+      sound: "default",
+      title: title,
+      body: body,
+      data: { data: "goes here" },
+      _displayInForeground: true,
+    };
+    const response = await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
   };
 
   handleChangespecialty = (v) => {
@@ -293,6 +370,9 @@ export default class index extends Component {
           Text="SEND REQUEST1"
           onPress={() => {
             this.handleSendPOST();
+            title = "New Request";
+            body = "New request from" + this.state.Name;
+            this.sendPushNotification(title, body); //Only for doctors
             Updates.reload();
           }}
         />
@@ -302,6 +382,15 @@ export default class index extends Component {
           onPress={() => {
             Linking.openURL("http://167.71.44.156:3000/main");
           }}
+        />
+        <Button
+          title={"Press to Send Notification"}
+          onPress={() =>
+            this.sendPushNotification(
+              "New Request",
+              "New request from" + this.state.Name
+            )
+          }
         />
       </View>
     );
